da9a26ae61110b4d90ffc00de67fa5305337df10
Tim <timmyg13@gmail.com>
2018-10-12 15:44:30 -0400

    add netlify auth

---
+++ src/views/Login.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="login">
+      <h1>Login</h1>
+      <section>
+        <button @click="login">login</button>
+      </section>
+  </div>
+</template>
+
+<script lang="ts">
+import { Vue } from "vue-property-decorator";
+// import * as netlifyIdentity from "netlify-identity-widget";
+const netlifyIdentity = require('netlify-identity-widget');
+
+// export default class Login extends Vue {
+//   mounted() {
+//     console.log("init");
+//     netlifyIdentity.init()
+//   }
+//   async login() {
+//     console.log("login");
+//     netlifyIdentity.open()
+//   }
+// }
+export default Vue.extend({
+  methods: {
+    async login() {
+      console.log("login");
+      netlifyIdentity.open()
+    }
+  },
+  mounted() {
+    console.log("init");
+    netlifyIdentity.init()
+  },
+})
+</script>
+ 
